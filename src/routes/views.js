5bcb022d070000edda9d01a8e0c50f19b6b983a7
maxi <maxinirbeat@gmail.com>
2025-02-22 15:14:50 -0300

    políticas de privacidad y terminos de uso añadidos. Se llevó el footer a la plantilla main

--- src/routes/views.js
+++ src/routes/views.js
@@ -2,20 +2,38 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
 
     res.render('index.handlebars', {
-        style : '/styles/naginattaz.min.css',
+        style: '/styles/naginattaz.min.css',
         indexStyle: '/styles/index.css',
-        images:{
+        images: {
             cart: '/images/cart.png',
             mainLogo: '/images/main-logo.png',
-            banners:{
-                mobile: {first: '/images/banners/BANNERPRINCIPAL_1_mob.png', second: '/images/banners/BANNERPRINCIPAL_2_mob.png'},
-                web: {first: '/images/banners/BANNERPRINCIPAL_1.png',second: '/images/banners/BANNERPRINCIPAL_2.png'}
+            banners: {
+                mobile: { first: '/images/banners/BANNERPRINCIPAL_1_mob.png', second: '/images/banners/BANNERPRINCIPAL_2_mob.png' },
+                web: { first: '/images/banners/BANNERPRINCIPAL_1.png', second: '/images/banners/BANNERPRINCIPAL_2.png' }
             }
         }
     });
 });
 
+router.get('/terms', async (req, res) => {
+
+    res.render('condition-terms.handlebars',{
+        style: '/styles/naginattaz.min.css',
+        legalStyle: '/styles/legals.css'
+    });
+});
+
+router.get('/policies', async(req, res) => {
+    res.render('policy.handlebars',{
+        style: '/styles/naginattaz.min.css',
+        legalStyle: '/styles/legals.css'
+    });
+});
+
+router.get('/login', async (req, res) => {
+    res.render()
+});
 export default router;
