a3c9b460fc50ec451562c6bf5e66de2353cfcf7a
maxi <maxinirbeat@gmail.com>
2025-03-12 18:07:16 -0300

    actualizado el puerto de escucha por defecto

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const environment = {
-    serverPort : process.env.PORT || 8080,
+    serverPort : process.env.PORT || 3000,
     database:{
         host : process.env.host || 'localhost',
         name: process.env.DB_NAME || 'naginattaz',
