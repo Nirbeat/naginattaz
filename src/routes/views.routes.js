6b089f5166a520503f19baf7d5de5e0fea684062
maxi <maxinirbeat@gmail.com>
2025-03-13 22:28:52 -0300

    agregado middleware para atajar errores de vista, redirige a en construccion

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UsersDAO } from "../database/DAO/UsersDAO.js";
 import { customVerification, ensureAuthenticated } from "../middlewares/authToken.js";
 import { CoursesDAO } from "../database/DAO/CoursesDAO.js";
+import { viewsRoutesErrorHandler } from "../middlewares/routes.js";
 
 const router = Router();
 
@@ -67,107 +68,138 @@ router.get('/team', async (req, res) => {
 
 router.use(ensureAuthenticated)
 
-router.get('/entrenamiento/:section?', async (req, res) => {
+router.get('/entrenamiento/:section?', async (req, res, next) => {
 
-    let [courses] = await new CoursesDAO().getAllCourses();
-    const { section } = req.params;
-    const { user } = req;
-
-    if (!section) { res.redirect('/entrenamiento/todas-las-clases') }
-
-    if (section == 'clases-individuales') {
-        courses = courses.filter(course => course.program == null)
+    try {
+        let [courses] = await new CoursesDAO().getAllCourses();
+        const { section } = req.params;
+        const { user } = req;
+    
+        if (!section) { res.redirect('/entrenamiento/todas-las-clases') }
+    
+        if (section == 'clases-individuales') {
+            courses = courses.filter(course => course.program == null)
+        }
+    
+        if (['programas', 'estilos', 'playlists', 'calendario', 'comunidad'].includes(section)) {
+            res.redirect('/construccion')
+        }
+    
+        res.render('training.handlebars', {
+            style: '/styles/main.css',
+            trainingStyle: '/styles/training.css',
+            userData: {
+                name: user.name,
+                profileImg: user.profile_image
+            },
+            courses
+        })
+        
+    } catch (error) {
+        next(error)
     }
-
-    if (['programas', 'estilos', 'playlists', 'calendario', 'comunidad'].includes(section)) {
-        res.redirect('/construccion')
-    }
-
-    res.render('training.handlebars', {
-        style: '/styles/main.css',
-        trainingStyle: '/styles/training.css',
-        userData: {
-            name: user.name,
-            profileImg: user.profile_image
-        },
-        courses
-    })
 });
 
-router.get('/clases/:courseID/:lessonID?', async (req, res) => {
+router.get('/clases/:courseID/:lessonID?', async (req, res, next) => {
     // console.log('Request received for courseID:', req.params.courseID);
 
-    const coursesDao = new CoursesDAO();
-    const { user } = req;
-    let { courseID, lessonID } = req.params;
-    const [lessons] = await coursesDao.getCourseLessonsById(courseID);
-
-    const courseOwned = user.ownedCoursesAndLessons.includes(parseInt(courseID))
-    console.log(user.role == "premium")
-    if (user.role != "premium" && !courseOwned) res.redirect('/store')
-    else {
-        // ESTO REDIRECCIONA AL PRIMER VIDEO DEL CURSO
-        if (!lessonID) {
-            lessonID = lessons[0].id;
-            res.status().redirect(`/clases/${courseID}/${lessonID}`)
-        }
+    try {
+        
+        const coursesDao = new CoursesDAO();
+        const { user } = req;
+        let { courseID, lessonID } = req.params;
+        const [lessons] = await coursesDao.getCourseLessonsById(courseID);
+    
+        const courseOwned = user.ownedCoursesAndLessons.includes(parseInt(courseID))
+        console.log(user.role == "premium")
+        if (user.role != "premium" && !courseOwned) res.redirect('/store')
         else {
-            const [[course]] = await coursesDao.getClassById(courseID);
-            res.render('lessons.handlebars', {
-                style: '/styles/main.css',
-                lessonsStyle: '/styles/lessons.css',
-                lessons,
-                courseName: course.class_name,
-                profileImg: user.profile_image,
-                currentLesson: function () {
-                    const current = lessons.find(lesson => lesson.id == lessonID);
-                    return current.lesson_url;
-                }
-            })
-        };
+            // ESTO REDIRECCIONA AL PRIMER VIDEO DEL CURSO
+            if (!lessonID) {
+                lessonID = lessons[0].id;
+                res.status().redirect(`/clases/${courseID}/${lessonID}`)
+            }
+            else {
+                const [[course]] = await coursesDao.getClassById(courseID);
+                res.render('lessons.handlebars', {
+                    style: '/styles/main.css',
+                    lessonsStyle: '/styles/lessons.css',
+                    lessons,
+                    courseName: course.class_name,
+                    profileImg: user.profile_image,
+                    currentLesson: function () {
+                        const current = lessons.find(lesson => lesson.id == lessonID);
+                        return current.lesson_url;
+                    }
+                })
+            };
+        }
+    } catch (error) {
+        next(error)
     }
 });
 
-router.get('/store', async (req, res) => {
+router.get('/store', async (req, res, next) => {
 
-    const [courses] = await new CoursesDAO().getAllCourses();
-
-    res.render('store.handlebars', {
-        style: '/styles/main.css',
-        storeStyle: '/styles/store.css',
-        courses,
-        images: {
-            banners: {
-                mobile: { first: '/images/banners/BANNERPRINCIPAL_1_mob.png', second: '/images/banners/BANNERPRINCIPAL_2_mob.png' },
-                web: { first: '/images/banners/BANNERPRINCIPAL_1.png', second: '/images/banners/BANNERPRINCIPAL_2.png', store: 'images/banners/BannerNag2.jpg' },
+    try {
+        
+        const [courses] = await new CoursesDAO().getAllCourses();
+    
+        res.render('store.handlebars', {
+            style: '/styles/main.css',
+            storeStyle: '/styles/store.css',
+            courses,
+            images: {
+                banners: {
+                    mobile: { first: '/images/banners/BANNERPRINCIPAL_1_mob.png', second: '/images/banners/BANNERPRINCIPAL_2_mob.png' },
+                    web: { first: '/images/banners/BANNERPRINCIPAL_1.png', second: '/images/banners/BANNERPRINCIPAL_2.png', store: 'images/banners/BannerNag2.jpg' },
+                }
             }
-        }
-    })
+        })
+    } catch (error) {
+        next (error)
+    }
 });
 
-router.get('/payment/:preferenceID', async (req, res) => {
-
+router.get('/payment/:preferenceID', async (req, res, next) => {
+try {
+    
     res.render('payment.handlebars', {
         style: "/styles/main.css"
 
     });
+} catch (error) {
+    next(error)
+}
 })
 
-router.get('/construccion', (req, res) => {
+router.get('/construccion', (req, res, next) => {
 
+    try {
+        
         res.render('construction.handlebars', {
             style: '/styles/main.css',
             constructionStyle: '/styles/construction.css',
             workerSvg: '/images/worker.svg'
         })
+    } catch (error) {
+        next(error)
+    }
     
 })
 
-router.get('*', (req, res) => {
-    res.render('construction.handlebars', {
-        style: '/styles/main.css',
-        constructionStyle: '/styles/construction.css',
-        workerSvg: '/images/worker.svg'
-    })
-})
+router.get('*', (req, res, next) => {
+    try {
+        
+        res.render('construction.handlebars', {
+            style: '/styles/main.css',
+            constructionStyle: '/styles/construction.css',
+            workerSvg: '/images/worker.svg'
+        })
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.use(viewsRoutesErrorHandler);
 export default router;
