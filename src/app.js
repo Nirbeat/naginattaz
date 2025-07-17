e42da5cb4842c1cdcfb13ff4149e813f4580881f
maxi <maxinirbeat@gmail.com>
2025-07-17 01:35:46 -0300

    restaurada vieja conexion a la DB

--- src/app.js
+++ src/app.js
@@ -53,13 +53,8 @@ app.use('/admin', adminRouter);
 app.use(viewsRouter);
 
 
-app.listen(environment.serverPort, () => {
-    console.log(`🚀 Express escuchando en puerto ${environment.serverPort}`);
-
-    setTimeout(() => {
-        DBConnection()
-            .then(conn => conn.connect())
-            .then(() => console.log("✅ DB conectada"))
-            .catch(err => console.error("❌ Fallo al conectar:", err.message));
-    }, 15000);
+app.listen(environment.serverPort, async () => {
+    (await DBConnection).connect()
+        .then(() => console.log("database connected"))
+        .catch((e) => console.log(e.message))
 });
--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,9 @@
 import mysql from 'mysql2/promise.js';
 import { environment } from '../config/env.js';
 
-
-export async function DBConnection(){
-    return await mysql.createConnection({
+export const DBConnection = await mysql.createConnection({
     host: environment.database.host,
     database: environment.database.name,
     password: environment.database.password,
     user: environment.database.user
-});}
+});
